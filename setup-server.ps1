cc8b928c382a503ea0d032953d067fe831181a21
Ankit Sharma <aankitsharmaa422@gmail.com>
2026-05-05 18:01:25 +0530

    Refactor setup script to use Install-App function for tool installation and improve error handling

--- setup-server.ps1
+++ setup-server.ps1
@@ -1,30 +1,47 @@
 # Fail fast
 $ErrorActionPreference = "Stop"
 
-Write-Host "Installing base tools..."
+function Install-App($id) {
+    Write-Host "Installing $id..."
+    winget install --id $id -e --source winget --accept-package-agreements --accept-source-agreements
+}
 
-winget install --id Git.Git -e --source winget
-winget install --id OpenJS.NodeJS.LTS -e
-winget install --id PostgreSQL.PostgreSQL -e
-winget install --id Nginx.Nginx -e
+try {
+    Write-Host "Installing base tools..."
 
-npm install -g pm2
+    Install-App "Git.Git"
+    Install-App "OpenJS.NodeJS.LTS"
+    Install-App "PostgreSQL.PostgreSQL"
+    Install-App "Nginx.Nginx"
 
-Write-Host "Checking Version"
+    Write-Host "Refreshing environment variables..."
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" +
+                [System.Environment]::GetEnvironmentVariable("Path","User")
 
-git -v
-node -v
-npm -v
-pm2 -v
-postgres -v
-nginx -v
+    Write-Host "Installing PM2..."
+    npm install -g pm2
 
+    Write-Host "Checking versions..."
 
-New-Item -ItemType Directory -Force -Path "C:\apps\insight"
-Set-Location "C:\apps\insight"
+    git --version
+    node -v
+    npm -v
+    pm2 -v
+    psql --version
+    nginx -v
 
-Write-Host "Cloning Backend Repo"
-git clone git@github.com:vishaltools-it/hana-insight.git backend
+    Write-Host "Setup completed successfully." -ForegroundColor Green
+}
+catch {
+    Write-Host "ERROR OCCURRED:" -ForegroundColor Red
+    Write-Host $_.Exception.Message -ForegroundColor Red
+}
 
-Write-Host "Cloning Frontend Repo"
-git clone git@github.com:vishaltools-it/hana-insight-ui.git frontend
+# New-Item -ItemType Directory -Force -Path "C:\apps\insight"
+# Set-Location "C:\apps\insight"
+
+# Write-Host "Cloning Backend Repo"
+# git clone git@github.com:vishaltools-it/hana-insight.git backend
+
+# Write-Host "Cloning Frontend Repo"
+# git clone git@github.com:vishaltools-it/hana-insight-ui.git frontend
